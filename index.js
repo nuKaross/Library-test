51d708c8be81baae18f3f5610aa0a65b02a2b599
nuKaross <127345332+nuKaross@users.noreply.github.com>
2023-09-21 21:26:38 +0200

    prototype to class refactor

--- index.js
+++ index.js
@@ -7,15 +7,20 @@ function Book(bookName, author, pages, read) {
   this.author = author
   this.pages = pages
   this.read = read
+}
 
-  this.isRead = function () {
-    return this.read ? 'read' : 'not read'
-  }
-  this.info = function () {
-    return `The book name is ${this.bookName} by ${this.author}, it has ${
-      this.pages
-    } pages and you have ${this.isRead()} it. `
-  }
+Book.prototype.isRead = function () {
+  return this.read ? 'read' : 'not read'
+}
+
+Book.prototype.info = function () {
+  return `The book name is ${this.bookName} by ${this.author}, it has ${
+    this.pages
+  } pages and you have ${this.isRead()} it. `
+}
+
+Book.prototype.toggleRead = function () {
+  this.read = !this.read
 }
 
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 235, false)
@@ -34,7 +39,8 @@ function addBook() {
   const bookName = document.getElementById('bookName').value
   const author = document.getElementById('author').value
   const pages = document.getElementById('pageNum').value
-  const isRead = document.querySelectorAll('.readCheckbox')
+  const isReadCheckbox = document.getElementById('isRead')
+  const isRead = isReadCheckbox.checked
   const newBook = new Book(bookName, author, pages, isRead)
 
   myLibrary.push(newBook)
@@ -50,9 +56,10 @@ function displayBooks() {
     let bookContainer = document.getElementById('bookContainer')
     let book = document.createElement('div')
     book.classList.add(`book`)
-    book.textContent = `Title: ${newBook.bookName}, Author: ${newBook.author}, Pages: ${newBook.pages}, Read: ${
-      newBook.isRead() ? 'Yes' : 'No'
-    }`
+    book.innerHTML = `<p><b>Title: ${newBook.bookName}</b></p> 
+    <p><b> Author: ${newBook.author}</b></p> 
+   <p> Pages: ${newBook.pages}</p> 
+    <p> Status: ${newBook.isRead()}</p> `
     book.setAttribute('data-index', index)
 
     const deleteButton = document.createElement('button')
@@ -66,8 +73,8 @@ function displayBooks() {
     readBtn.textContent = 'Read Check'
     readBtn.classList.add('readBtn')
     readBtn.addEventListener('click', () => {
-      readSwitch(parseInt(index))
-    }) // NEED TO ADD HERE WHAT TO SEND TO 'read' FUNCTION FOR SWITCHING
+      toggleRead(parseInt(index))
+    })
     book.appendChild(readBtn)
     book.appendChild(deleteButton)
     bookContainer.appendChild(book)
@@ -81,6 +88,8 @@ function deleteBook(index) {
 }
 
 //READ
-function readSwitch(index) {
+
+function toggleRead(index) {
+  myLibrary[index].toggleRead()
   displayBooks()
 }
